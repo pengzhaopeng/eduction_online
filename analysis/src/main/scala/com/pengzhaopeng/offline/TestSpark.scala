3377a4774cb2e211fc9a3ceb922d7589aa774558
pengzhaopeng <pengzhaopeng@aliyun.com>
2020-02-24 23:51:41 +0800

    首次修改代码提交

--- analysis/src/main/scala/com/pengzhaopeng/offline/TestSpark.scala
+++ analysis/src/main/scala/com/pengzhaopeng/offline/TestSpark.scala
@@ -11,7 +11,7 @@ import com.pengzhaopeng.utils.JodaTimeUtil
 object TestSpark {
   def main(args: Array[String]): Unit = {
 
-    val time: Long = JodaTimeUtil.getStartOfDay()
+    val time: Long = JodaTimeUtil.getEndOfDay()
     println(time)
   }
 }
